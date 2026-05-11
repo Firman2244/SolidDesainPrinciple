edb0adbbf0b9e951c3f01bb336252f1c9eb6a867
Deca Finandi Ananda <145514976+TutupKaleng@users.noreply.github.com>
2026-05-11 19:45:39 +0700

    DIP (Dependency Inversion Principle)
    Refactor UserService to use dependency injection for database connections. Added PostgreSQLDatabase implementation.

--- Deca.java
+++ Deca.java
@@ -1,14 +1,35 @@
-class MySQLDatabase {
+// Abstraction / Interface
+interface Database {
+    void connect();
+}
 
+// Implementasi MySQL
+class MySQLDatabase implements Database {
+
+    @Override
     public void connect() {
         System.out.println("Terhubung ke MySQL Database");
     }
 }
 
+// Implementasi database lain (opsional)
+class PostgreSQLDatabase implements Database {
+
+    @Override
+    public void connect() {
+        System.out.println("Terhubung ke PostgreSQL Database");
+    }
+}
+
+// UserService sekarang bergantung pada abstraction
 class UserService {
 
-    
-    private MySQLDatabase database = new MySQLDatabase();
+    private Database database;
+
+    // Dependency Injection melalui constructor
+    public UserService(Database database) {
+        this.database = database;
+    }
 
     public void saveUser() {
 
@@ -16,14 +37,22 @@ class UserService {
 
         System.out.println("Data user berhasil disimpan");
     }
-};
+}
 
-public class Deca {
+public class Main {
 
     public static void main(String[] args) {
 
-        UserService service = new UserService();
+        // Bisa menggunakan MySQL
+        Database mysql = new MySQLDatabase();
+        UserService service1 = new UserService(mysql);
+        service1.saveUser();
 
-        service.saveUser();
+        System.out.println();
+
+        // Bisa diganti PostgreSQL tanpa mengubah UserService
+        Database postgres = new PostgreSQLDatabase();
+        UserService service2 = new UserService(postgres);
+        service2.saveUser();
     }
-};
+}
